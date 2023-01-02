23063e8f0fed7ba2f1cf529d95a42483e18e5ffc
Kirti Purbia <purbiakirti@gmail.com>
2023-01-02 10:32:11 +0530

    removed db url

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb://admin:YOIbMbwMGXYnPKXK@ac-zvouvz3-shard-00-02.9yaphws.mongodb.net:27017/MyDB", // "mongodb://lilgaming:PKS3emn55y@129.154.227.212:30034/lil-gaming"
+    // mongoURI: "mongodb://admin:YOIbMbwMGXYnPKXK@ac-zvouvz3-shard-00-02.9yaphws.mongodb.net:27017/MyDB", // "mongodb://lilgaming:PKS3emn55y@129.154.227.212:30034/lil-gaming"
     // "mongodb+srv://<username>:<password>@mycluster.9yaphws.mongodb.net/?retryWrites=true&w=majority"
     // "mongodb://admin:YOIbMbwMGXYnPKXK@ac-zvouvz3-shard-00-02.9yaphws.mongodb.net:27017/MyDB"
     // mongodb+srv://admin:<password>@mycluster.9yaphws.mongodb.net/test
